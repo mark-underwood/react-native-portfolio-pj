c9e55ccd98a40940c8a434ecc6d18ba91a1c17b0
Mark Underwood <mark.thunderwood@gmail.com>
2024-07-05 14:07:25 -0500

    Added Moon stub file. Redux Store comment typo fix.

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { leaderboardReducer } from '../features/leaderboard/leaderboardSlice';
 import { actionButtonsReducer } from '../components/actionButtons/actionButtonsSlice';
-// impore reducers from relevant xSlice
+// import reducers from relevant xSlice
 
 export const store = configureStore({
     reducer: {
