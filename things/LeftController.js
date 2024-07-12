f8ca93fac302be98290ecf969bffa220728df3a4
Mark Underwood <mark.thunderwood@gmail.com>
2024-07-11 19:29:24 -0500

    Controller corner rounding 1

--- things/LeftController.js
+++ things/LeftController.js
@@ -6,60 +6,43 @@ import SButtonLeft from '../components/actionButtons/SButtonLeft';
 import SButtonUp from '../components/actionButtons/SButtonUp';
 
 const LeftController = () => {
-
+  
   return (
     <View style={{
       flex: 1,
-      // aspectRatio: 0.5,
-      // minHeight: {shortDim},
-      backgroundColor: "#370d11"
+      height: '100%',
+      width: '100%',
+      borderStartStartRadius: 64,
+      borderEndStartRadius: 64,
+      backgroundColor: '#370d11'
     }}>
-      <View style={styles.container}>
-        <Text style={{color: "white", alignSelf: 'center'}}>\ L-Controller \</Text>
-        <View style={styles.row}>{/* row 1 */}
-          <View style={styles.col}>{/* row 1 | col 1 */}
-            <SButtonLeft />
-          </View>
-          <View style={styles.col}>{/* row 1 | col 2 */}
-            <Text> </Text>
-          </View>
+      <Text style={{color: 'white', alignSelf: 'center'}}>\ L-Controller \</Text>
+      <View style={styles.row}>{/* row 1 */}
+        <SButtonLeft style={styles.col} />{/* row 1 | col 1 */}
+        <View style={styles.col}>{/* row 1 | col 2 */}
+          {/* <Text> </Text> */}
         </View>
-        <View style={styles.row}>{/* row 2 */}
-          <View style={styles.col}>{/* row 2 | col 1 */}
-            <Text> </Text>
-          </View>
-          <View style={styles.col}>{/* row 2 | col 2 */}
-            <SButtonUp />
-          </View>
+      </View>
+      <View style={styles.row}>{/* row 2 */}
+        <View style={styles.col}>{/* row 2 | col 1 */}
+          {/* <Text> </Text> */}
         </View>
-        <View style={styles.row}>{/* row 3 */}
-          {/* intentionally blank - for now */}
-        </View>
+        <SButtonUp style={styles.col} />{/* row 2 | col 2 */}
+      </View>
+      <View style={styles.row}>{/* row 3 */}
+        {/* intentionally blank - for now */}
       </View>
     </View>
   );
 };
 
 const styles = EStyleSheet.create({
-  container: {
-    flex: 1,
-    // borderStartStartRadius: '1rem',
-    borderTopLeftRadius: 64,
-    // borderEndStartRadius: '1rem',
-    borderBottomLeftRadius: 64,
-    borderBottomRightRadius: 100,
-    borderTopRightRadius: 100//,
-    // outerWidth: '100%',
-    // outerHeight: '100%'
-  },
   row: {
     flex: 1,
     flexDirection: 'row'
-    // flexDirection: 'column' 
   },
   col: {
     flex: 1,
-    // flexDirection: 'row',
     aspectRatio: 1,
     alignItems: 'center',
     justifyContent: 'center'
--- things/RightController.js
+++ things/RightController.js
@@ -10,44 +10,33 @@ const RightController = () => {
   return (
     <View style={{
       flex: 1,
-      // aspectRatio: 0.5,
-      // minHeight: {shortDim},
-      backgroundColor: "#03333c"
+      height: '100%',
+      width: '100%',
+      borderStartEndRadius: 64,
+      borderEndEndRadius: 64,
+      backgroundColor: '#03333c'
     }}>
-      <View style={styles.container}>
-        <Text style={{color: "white", alignSelf: 'center'}}>/ R-Controller /</Text>
-        <View style={styles.row}>{/* row 1 */}
-          <View style={styles.col}>{/* row 1 | col 1 */}
-            <Text> </Text>
-          </View>
-          <View style={styles.col}>{/* row 1 | col 2 */}
-            <EButtonRight />
-          </View>
+      <Text style={{color: 'white', alignSelf: 'center'}}>/ R-Controller /</Text>
+      <View style={styles.row}>{/* row 1 */}
+        <View style={styles.col}>{/* row 1 | col 1 */}
+          {/* <Text> </Text> */}
         </View>
-        <View style={styles.row}>{/* row 2 */}
-          <View style={styles.col}>{/* row 2 | col 1 */}
-            <EButtonUp />
-          </View>
-          <View style={styles.col}>{/* row 2 | col 2 */}
-              <Text> </Text>
-            </View>
+        <EButtonRight style={styles.col} />{/* row 1 | col 2 */}
+      </View>
+      <View style={styles.row}>{/* row 2 */}
+        <EButtonUp style={styles.col} />{/* row 2 | col 1 */}
+        <View style={styles.col}>{/* row 2 | col 2 */}
+            {/* <Text> </Text> */}
         </View>
-        <View style={styles.row}>{/* row 3 */}
-          {/* intentionally blank - for now */}
-        </View>
+      </View>
+      <View style={styles.row}>{/* row 3 */}
+        {/* intentionally blank - for now */}
       </View>
     </View>
   );
 };
 
 const styles = EStyleSheet.create({
-  container: {
-    flex: 1,
-    borderTopRightRadius: '1rem',
-    borderBottomRightRadius: '1rem',
-    outerWidth: '100%',
-    outerHeight: '100%'
-  },
   row: {
     flex: 1,
     flexDirection: 'row'
